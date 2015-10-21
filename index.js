7a59d2726e7191028ca20891c5d92d1d0ed00104
Damien Maillard <dmaillard06@gmail.com>
2015-10-21 21:39:19 +0200

    first version

---
+++ index.js
@@ -0,0 +1,11 @@
+import map from './lib/iterable-map.js';
+import filter from './lib/iterable-filter.js';
+import reduceToThenable from './lib/iterable-reduce-thenable.js';
+
+var exports = {
+	map: map,
+	filter: filter,
+	reduceToThenable: reduceToThenable
+};
+
+export default exports;
---
+++ test/0-filter-thenable.js
@@ -0,0 +1,5 @@
+import filterThenable from '../lib/iterable-filter-thenable.js';
+
+export function suite(aad){
+	
+}
