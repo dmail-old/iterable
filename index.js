66cbd0ec9b5d964a5fd3ff8fc3d7ee292ddd61ee
Damien Maillard <dmaillard06@gmail.com>
2015-11-02 14:28:22 +0100

    add condition to reduceThenable & until method

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import map from './lib/iterable-map.js';
 import filter from './lib/iterable-filter.js';
 import reduceToThenable from './lib/iterable-reduce-thenable.js';
+import until from './lib/iterable-until.js';
 
 var exports = {
 	map: map,
 	filter: filter,
+	until: until,
 	reduceToThenable: reduceToThenable
 };
 
